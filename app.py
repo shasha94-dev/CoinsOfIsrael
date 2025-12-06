01327215a8c42d396749834bb488beec13712a61
Shahar Shamai <shasha94@gmail.com>
2025-12-06 13:40:33 +0200

    Fix deploy

--- app.py
+++ app.py
@@ -141,4 +141,4 @@ def serve_thumbnail(filename):
     return send_from_directory(THUMB_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=5000)
